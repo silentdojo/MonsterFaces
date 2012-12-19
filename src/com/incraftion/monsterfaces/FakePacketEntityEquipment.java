493b66aebe157a4e1b5712c4f8c41a2239f262a6
Bryan Mendish <silentdojo@incraftion.com>
2012-12-19 02:41:31 -0500

    Removed PlayerHeads depend

--- src/com/incraftion/monsterfaces/FakePacketEntityEquipment.java
+++ src/com/incraftion/monsterfaces/FakePacketEntityEquipment.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.Skull;
 import org.bukkit.plugin.Plugin;
-import org.shininet.bukkit.playerheads.Skull;
 
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
@@ -16,6 +20,7 @@ public class FakePacketEntityEquipment implements Runnable {
 	private ProtocolManager protocolManager;
 	private Player player;
 	private int uid;
+	private ItemStack skull;
 	
 	public FakePacketEntityEquipment(Plugin plugin, ProtocolManager protocolManager, Player player, int uid) {
 		this.plugin = plugin;
@@ -31,10 +36,11 @@ public class FakePacketEntityEquipment implements Runnable {
 		int amt = plugin.getConfig().getStringList("playernames").size();
 		int prand = nrand.nextInt(amt);
 		List<String> playernames = plugin.getConfig().getStringList("playernames");
-	
+		ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
+		((SkullMeta) skull.getItemMeta()).setOwner(playernames.get(prand));
 		try {
 			packet.getSpecificModifier(int.class).write(0, uid).write(1, 4);
-			packet.getItemModifier().write(0, new Skull(playernames.get(prand)).getItemStack());
+			packet.getItemModifier().write(0, skull);
 			protocolManager.sendServerPacket(player, packet);
 		} catch (Exception e) {
 			plugin.getLogger().log(Level.SEVERE, "Couldn't send fake EntityEquipment packet.", e);
